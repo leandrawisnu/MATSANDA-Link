e9bb9cf6f0038d7cc39d1afcbc633ea336070528
Leandra <andrawisnu60@gmail.com>
2025-05-05 11:37:56 +0700

    Home Backend Done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt")
 }
 
 android {
@@ -45,4 +46,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    // Glide main library
+    implementation("com.github.bumptech.glide:glide:4.14.2")
+    // Glide compiler for annotation processing
+    kapt("com.github.bumptech.glide:compiler:4.14.2")
 }
--- app/src/main/java/com/example/matsandaplus/HomeFragment.kt
+++ app/src/main/java/com/example/matsandaplus/HomeFragment.kt
@@ -1,10 +1,29 @@
 package com.example.matsandaplus
 
+import android.annotation.SuppressLint
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONArray
+import org.json.JSONObject
+import java.net.HttpURLConnection
+import java.net.URL
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,6 +54,70 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        refresh()
+    }
+
+    fun refresh() {
+        val recyclerView = view?.findViewById<RecyclerView>(R.id.headline_rv)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val connection = URL("http://tour-occupational.gl.at.ply.gg:32499/api/News").openConnection() as HttpURLConnection
+            connection.requestMethod = "GET"
+
+            val whole = connection.inputStream.bufferedReader().readText()
+            val response = JSONObject(whole)
+            val data = response.getJSONArray("data")
+
+            withContext(Dispatchers.Main) {
+                recyclerView?.layoutManager = LinearLayoutManager(view?.context, LinearLayoutManager.HORIZONTAL, false)
+                recyclerView?.adapter = AdapterRV(data)
+            }
+        }
+    }
+
+    class AdapterRV(private val data : JSONArray) : RecyclerView.Adapter<AdapterRV.Holder>(){
+        class Holder(private val view: View) : RecyclerView.ViewHolder(view.rootView) {
+            @SuppressLint("SetTextI18n")
+            fun bind (item: JSONObject) {
+
+                val formatter = DateTimeFormatter.ofPattern("dd MMMM yyyy", Locale("id"))
+                val judul = item.getString("title")
+
+                if(judul.length>55) {
+                    view.findViewById<TextView>(R.id.judul_headline_TV).text = judul.substring(0, 54) + "..."
+                } else {
+                    view.findViewById<TextView>(R.id.judul_headline_TV).text = judul
+                }
+
+                view.findViewById<TextView>(R.id.date_headline_TV).text = formatter.format(LocalDate.parse(item.getString("createdAt")))
+                val gambar = view.findViewById<ImageView>(R.id.berita_headline_image)
+
+                Glide.with(view.context)
+                    .load(item.getString("image"))
+                    .into(gambar)
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+            val inflater =
+                LayoutInflater.from(parent.context).inflate(R.layout.berita_rv_layout, parent, false)
+            val view = Holder(inflater)
+            return view
+        }
+
+        override fun getItemCount(): Int {
+            return data.length()
+        }
+
+        override fun onBindViewHolder(holder: Holder, position: Int) {
+            val item = data.getJSONObject(position)
+            holder.bind(item)
+        }
     }
 
     companion object {
